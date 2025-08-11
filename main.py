839ba642df32d4cfd03abfe8421416494881e9ea
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-08-11 15:21:41 -0300

    feat: extend general options

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from scripts import renamer, lister, zipper
+from scripts import file_lister, file_renamer, file_zipper, img_converter
 
 TOOLS = {
-    "1": ("Renomear arquivos com padrão", renamer.run),
-    "2": ("Listar arquivos", lister.run),
-    "3": ("Compactar arquivos", zipper.run),
+    "1": ("Renomear arquivos com padrão", file_renamer.run),
+    "2": ("Listar arquivos", file_lister.run),
+    "3": ("Compactar arquivos", file_zipper.run),
+    "4": ("Converter imagens", img_converter.run),
 }
 
 def get_tool(choice):
